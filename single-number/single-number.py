bb8ba6bad8d6fcbb72d62d17681515af0cd265c4
Airvw <46955620+Airvw@users.noreply.github.com>
2021-10-31 00:00:42 +0900

    Time: 227 ms (24.43%), Space: 16.9 MB (19.57%) - LeetHub

---
+++ single-number/single-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        d = {}
+        for num in nums:
+            if num in d:
+                d[num] += 1
+            else:
+                d[num] = 1
+        print(d)
+        for key, value in d.items():
+            if value == 1:
+                return key
+        
+        
