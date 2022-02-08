9da30aaffaba2a114e06b638945f45263b03cd97
Airvw <46955620+Airvw@users.noreply.github.com>
2022-02-08 19:55:06 +0900

    Time: 95 ms (18.86%), Space: 14.7 MB (93.99%) - LeetHub

---
+++ 414-third-maximum-number/414-third-maximum-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums.sort(reverse = True)
+        cnt = 1
+        Max = nums[0]
+        curr = Max
+        for i in nums[1:]:
+            print(i, curr)
+            if i != curr:
+                cnt += 1
+                curr = i
+            if cnt == 3:
+                break
+        if cnt == 3:
+            return curr
+        else:
+            return Max
