7c9ccd4ba21308ec4f70e8d0945637f273917ac5
Airvw <46955620+Airvw@users.noreply.github.com>
2022-03-24 19:24:37 +0900

    Time: 797 ms (17.93%), Space: 20.8 MB (80.59%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        i, j = 0, len(people) - 1
+        answer = 0
+        while i <= j:
+            hap = people[i] + people[j]
+            if hap > limit:
+                j -= 1
+                answer += 1
+            elif hap <= limit:
+                answer += 1
+                i += 1
+                j -= 1
+        return answer
+            
