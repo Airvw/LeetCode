1efbe54a13d70d8cabafc2b6c35141137c43a0ed
Airvw <46955620+Airvw@users.noreply.github.com>
2022-03-24 19:25:48 +0900

    Time: 523 ms (70.06%), Space: 20.8 MB (80.59%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.py
+++ 881-boats-to-save-people/881-boats-to-save-people.py
@@ -7,10 +7,9 @@ class Solution:
             hap = people[i] + people[j]
             if hap > limit:
                 j -= 1
-                answer += 1
             else:
-                answer += 1
                 i += 1
                 j -= 1
+            answer += 1
         return answer
             
