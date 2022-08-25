24c2ca0cd60581618e1c9443c9bafbae22ce2240
Airvw <46955620+Airvw@users.noreply.github.com>
2022-08-25 23:17:22 +0900

    Time: 35 ms (7.09%), Space: 50.3 MB (6.36%) - LeetHub

--- 326-power-of-three/326-power-of-three.java
+++ 326-power-of-three/326-power-of-three.java
@@ -1,11 +1,15 @@
 class Solution {
+    int[] dp = new int[32];
     public boolean isPowerOfThree(int n) {
         if(n == 1){
             return true;
         }
-        if(n % 3 != 0 || n == 0){
-            return false;
+        dp[0] = 1;
+        for (int i = 1; i < 32; i++) {
+            dp[i] = dp[i - 1] * 3;
+            if (dp[i] == n) return true;
+            if (dp[i] > n) return false;
         }
-        return isPowerOfThree(n / 3);
+        return false;
     }
 }
