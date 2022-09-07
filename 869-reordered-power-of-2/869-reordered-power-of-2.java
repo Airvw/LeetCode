c5e156896b8803c13c0e91c5d896f3c1a92c1f5f
Airvw <46955620+Airvw@users.noreply.github.com>
2022-09-07 19:35:23 +0900

    Time: 369 ms (5.02%), Space: 41 MB (57.44%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.java
+++ 869-reordered-power-of-2/869-reordered-power-of-2.java
@@ -1,23 +1,45 @@
-import java.util.Arrays;
-
 class Solution {
     public boolean reorderedPowerOf2(int n) {
-        int[] numArr = count(n);
-        for(int i = 0; i < 31; i++){
-            if(Arrays.equals(numArr, count(1 << i))){
-                return true;
-            }
+        String s = Integer.toString(n);
+        int[] arr = new int[s.length()];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = s.charAt(i) - '0';
+        }
+        return permutation(arr, 0);
+    }
+
+    private boolean permutation(int[] arr, int start){
+        if(arr.length == start)
+            return isPowerOfTwo(arr);
+
+        for(int i = start; i < arr.length; i++){
+            swap(arr, start, i);
+
+            if(permutation(arr, start + 1)) return true;
+
+            swap(arr, start, i);
         }
 
         return false;
     }
 
-    public int[] count(int n) {
-        int[] result = new int[10];
-        while(n > 0){
-            result[n % 10]++;
-            n /= 10;
+    private void swap(int[] arr, int start, int i) {
+        int t = arr[start];
+        arr[start] = arr[i];
+        arr[i] = t;
+    }
+
+    private boolean isPowerOfTwo(int[] arr){
+        if(arr[0] == 0) return false;
+        int result = 0;
+        for(int x : arr){
+            result = 10 * result + x;
         }
-        return result;
+
+        while(result > 0 && ((result & 1) == 0)){
+            result >>= 1;
+        }
+
+        return result == 1;
     }
 }
