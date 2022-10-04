cae1d966b009aa13f50356147c55e2097363d9dd
Airvw <46955620+Airvw@users.noreply.github.com>
2022-10-04 15:26:38 +0900

    Time: 1 ms (69.13%), Space: 44 MB (27.56%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -1,33 +1,9 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
+
+class Solution{
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root == null) return false;
-        if(recur(root, targetSum))
-            return true;
-        return false;
-    }
-
-    private boolean recur(TreeNode root, int targetSum) {
-        if(root.left == null && root.right == null) return (targetSum - root.val) == 0;
-        if(root.left == null)
-            return recur(root.right, targetSum - root.val);
-        if(root.right == null) {
-            return recur(root.left, targetSum - root.val);
-        }
-        return recur(root.left, targetSum - root.val) || recur(root.right, targetSum - root.val);
+        if(root.left == null && root.right == null) return root.val == targetSum;
+        targetSum -= root.val;
+        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
     }
 }
