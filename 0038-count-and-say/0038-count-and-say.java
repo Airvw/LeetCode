71ac1384b80a59f3f97c50f18c24f1e370d6bd8e
Airvw <46955620+Airvw@users.noreply.github.com>
2022-10-18 20:07:46 +0900

    Time: 4 ms (85.69%), Space: 40.9 MB (91.68%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -2,25 +2,22 @@ class Solution {
     public String countAndSay(int n) {
         if(n == 1) return "1";
         StringBuilder sb = new StringBuilder();
-        int[] checkCnt = new int[10];
         String str = countAndSay(n - 1);
 
         char prev = str.charAt(0);
+        int cnt = 0;
         for(char ch : str.toCharArray()){
             if(prev != ch){
-                sb.append(checkCnt[prev - '0']);
-                sb.append(prev);
-                checkCnt[prev - '0'] = 0;
-                checkCnt[ch - '0']++;
+                sb.append(cnt).append(prev);
                 prev = ch;
+                cnt = 1;
             }
             else {
-                checkCnt[ch - '0']++;
+                cnt++;
             }
         }
-        if(checkCnt[prev - '0'] > 0){
-            sb.append(checkCnt[prev - '0']);
-            sb.append(prev);
+        if(cnt > 0){
+            sb.append(cnt).append(prev);
         }
 
         return sb.toString();
