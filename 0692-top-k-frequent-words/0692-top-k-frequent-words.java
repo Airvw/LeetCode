df4d08e406865baa96004acee9c82749a8de5a5c
Airvw <46955620+Airvw@users.noreply.github.com>
2022-10-20 16:00:15 +0900

    Time: 10 ms (75.92%), Space: 45 MB (67.29%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,28 +1,23 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-        HashMap<String, Integer> map = new HashMap<>();
-        int maxCnt = 0;
+        List<String> ans = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
         for(String word : words){
             map.put(word, map.getOrDefault(word, 0) + 1);
-            maxCnt = Math.max(maxCnt, map.get(word));
         }
-        List<String>[] cntArr = new ArrayList[maxCnt + 1];
-        for(Map.Entry<String, Integer> entry : map.entrySet()){
-            int cntIdx = entry.getValue();
-            if(cntArr[cntIdx] == null){
-                cntArr[cntIdx] = new ArrayList<>();
-            }
-            cntArr[cntIdx].add(entry.getKey());
-        }
-        List<String> ans = new ArrayList<>();
 
-        for(int i = maxCnt; i >= 0 && ans.size() < k; i--){
-            if(cntArr[i] != null){
-                Collections.sort(cntArr[i]);
-                ans.addAll(cntArr[i]);
-            }
-        }
+        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(
+                (a, b) -> a.getValue() == b.getValue() ? a.getKey().compareTo(b.getKey()) : b.getValue() - a.getValue()
+        );
         
-        return ans.subList(0, k);
+        for(Map.Entry<String,Integer> entry : map.entrySet()){
+            pq.add(entry);
+        }
+
+        while(k-- > 0){
+            ans.add(pq.poll().getKey());
+        }
+
+        return ans;
     }
 }
