3e47d54ccc338453e6e512e1a2d2b5a82e822ade
Airvw <46955620+Airvw@users.noreply.github.com>
2022-11-23 20:00:28 +0900

    Time: 21 ms (32.61%), Space: 43.5 MB (81.66%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,24 +1,16 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        int n = 9;
-        HashSet<Character>[] setRow = new HashSet[n];
-        HashSet<Character>[] setCol = new HashSet[n];
-        HashSet<Character>[] setSquare = new HashSet[n];
-
-        for(int i = 0; i <9; i++){
-            setRow[i] = new HashSet<>();
-            setCol[i] = new HashSet<>();
-            setSquare[i] = new HashSet<>();
-        }
-
-        for(int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
+        Set<String> set = new HashSet<>();
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
                 char ch = board[i][j];
-                if (ch != '.') {
-                    if(!setRow[i].add(ch) || !setCol[j].add(ch) || !setSquare[(i / 3) * 3 + j / 3].add(ch)) return false;
+                if(ch != '.'){
+                    String val = "(" + ch + ")";
+                    if(!set.add(i + val) || !set.add(val + j) || !set.add(i / 3 + val + j / 3)) return false;
                 }
             }
         }
+
         return true;
     }
 }
